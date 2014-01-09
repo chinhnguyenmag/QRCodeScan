c51407c74e9f1a7fc78b70c355d916e56fe79314
Vu Le <magrabbit@magrabbit-To-be-filled-by-O-E-M.(none)>
2014-01-09 10:56:42 +0700

    apply layout

--- Code/QRCodeScan/src/com/magrabbit/qrcodescan/activity/MainActivity.java
+++ Code/QRCodeScan/src/com/magrabbit/qrcodescan/activity/MainActivity.java
@@ -3,19 +3,20 @@ package com.magrabbit.qrcodescan.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.magrabbit.qrcodescan.R;
 
 public class MainActivity extends Activity {
 	SlidingMenu menu = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		initMenu();
 	}
+
 	public void onClick_Menu(View view) {
 		if (menu == null) {
 			initMenu();
