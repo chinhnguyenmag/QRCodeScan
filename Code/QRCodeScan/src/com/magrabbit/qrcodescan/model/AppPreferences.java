55e4313b6f9ebec327e8f048d909285d8267ab18
Hung Hoang <hunghoang@magrabbit.com>
2014-01-13 15:53:58 +0700

    hunghoang

--- Code/QRCodeScan/src/com/magrabbit/qrcodescan/model/AppPreferences.java
+++ Code/QRCodeScan/src/com/magrabbit/qrcodescan/model/AppPreferences.java
@@ -25,4 +25,48 @@ public class AppPreferences {
 		String server = mAppSharedPrefs.getString("Server", "");
 		return server;
 	}
+
+	public boolean setSound(boolean isSound) {
+		mPrefsEditor.putBoolean("Sound", isSound);
+		mPrefsEditor.commit();
+		return true;
+	}
+
+	public boolean isSound() {
+		boolean isSound = mAppSharedPrefs.getBoolean("Sound", false);
+		return isSound;
+	}
+
+	public boolean setOpenUrl(boolean isOpen) {
+		mPrefsEditor.putBoolean("OpenUrl", isOpen);
+		mPrefsEditor.commit();
+		return true;
+	}
+
+	public boolean isOpenUrl() {
+		boolean isOpen = mAppSharedPrefs.getBoolean("OpenUrl", false);
+		return isOpen;
+	}
+
+	public boolean setProfileUrl(String profileUrl) {
+		mPrefsEditor.putString("ProfileUrl", profileUrl);
+		mPrefsEditor.commit();
+		return true;
+	}
+
+	public String getProfileUrl() {
+		String profileUrl = mAppSharedPrefs.getString("ProfileUrl", "");
+		return profileUrl;
+	}
+
+	public boolean setCloseUrl(boolean isClose) {
+		mPrefsEditor.putBoolean("CloseUrl", isClose);
+		mPrefsEditor.commit();
+		return true;
+	}
+
+	public boolean getCloseUrl() {
+		boolean isClose = mAppSharedPrefs.getBoolean("CloseUrl", false);
+		return isClose;
+	}
 }
